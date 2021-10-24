9902a0fb3928ccdac19a1b869a9a3671f7127ab3
AniruddhaSoni <anirudh.soni007@gmail.com>
2021-10-24 10:17:58 +0530

    home yojna comps

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-// import Yojna from "../yojna/Yojna";
+import Yojna from "../yojna/Yojna";
 import useFetch from "../../useFetch";
+import "./home.css";
 
 const url = "https://schema-eight.vercel.app/api/allyojna";
 
 export default function Home() {
   const { data: yojnas, error, isPending } = useFetch(url);
   return (
-    <div>
+    <div className="home">
       {isPending && <div>Loading...</div>}
       {error && <div>{error}</div>}
-      {yojnas && <div>loded</div>}
+      {yojnas &&
+        yojnas.map((yojna) => (
+          <Yojna
+            key={yojna._id}
+            thumbnail={yojna.Link}
+            title={yojna.Name}
+            desc={yojna.information}
+          ></Yojna>
+        ))}
     </div>
   );
 }
--- src/components/yojna/Yojna.jsx
+++ src/components/yojna/Yojna.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import "./yojna.css";
 
-export default function Yojna() {
-  return <div>Yojna sample</div>;
+export default function Yojna(data) {
+  return (
+    <div className="yojna">
+      <img src={data.thumbnail} alt="" className="yojna_thumbnail" />
+      <div className="yojna_title">{data.title}</div>
+      <div className="yojna_desc">{data.desc}</div>
+      <button>Read More</button>
+    </div>
+  );
 }
