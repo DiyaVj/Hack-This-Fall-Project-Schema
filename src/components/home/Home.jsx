3ab6c0356616491c0c4abdf7ab9bab117ffa86bf
AniruddhaSoni <anirudh.soni007@gmail.com>
2021-10-24 10:27:38 +0530

    tagline

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -8,18 +8,21 @@ const url = "https://schema-eight.vercel.app/api/allyojna";
 export default function Home() {
   const { data: yojnas, error, isPending } = useFetch(url);
   return (
-    <div className="home">
-      {isPending && <div>Loading...</div>}
-      {error && <div>{error}</div>}
-      {yojnas &&
-        yojnas.map((yojna) => (
-          <Yojna
-            key={yojna._id}
-            thumbnail={yojna.Link}
-            title={yojna.Name}
-            desc={yojna.information}
-          ></Yojna>
-        ))}
+    <div>
+      <div className="tagline">Find Schemes/Yojnas that meet your needs!</div>
+      <div className="home">
+        {isPending && <div>Loading...</div>}
+        {error && <div>{error}</div>}
+        {yojnas &&
+          yojnas.map((yojna) => (
+            <Yojna
+              key={yojna._id}
+              thumbnail={yojna.Link}
+              title={yojna.Name}
+              desc={yojna.information}
+            ></Yojna>
+          ))}
+      </div>
     </div>
   );
 }
